69cb320f3e574a09369eba80e177a1af78e26c4e
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-08 06:34:38 +0000

    [Finish]4-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,5 +4,10 @@ class PostImage < ApplicationRecord
   attachment :image
 
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
 end
